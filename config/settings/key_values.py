74a002116477bd128017aa4135d5f1f1be514822
shamspias <shamsuddinpias0@gmail.com>
2023-05-09 14:28:20 +0600

    fix: settings

---
+++ config/settings/key_values.py
@@ -0,0 +1,23 @@
+import os
+
+# Celery
+BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379')
+
+
+# Open AI key
+OPENAI_API_KEY = os.getenv('OPEN_AI_KEY')
+
+# Pinecone
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENVIRONMENT = os.getenv('PINECONE_ENVIRONMENT')
+PINECONE_INDEX_NAME = os.getenv('PINECONE_INDEX_NAME')
+PINECONE_NAMESPACE_NAME = os.getenv('PINECONE_NAMESPACE_NAME')
+
+# Admin Site Config
+ADMIN_SITE_HEADER = os.getenv('ADMIN_SITE_HEADER')
+ADMIN_SITE_TITLE = os.getenv('ADMIN_SITE_TITLE')
+ADMIN_SITE_INDEX = os.getenv('ADMIN_SITE_INDEX')
+
+# OAuth2 settings
+APPLICATION_NAME = os.getenv('APPLICATION_NAME', 'chatbot')
