ad73cb7b176b756bbcf2d94d3171048abbe226e3
shamspias <shamsuddinpias0@gmail.com>
2023-05-13 01:17:29 +0600

    fix: language

--- ycla_chat/tasks.py
+++ ycla_chat/tasks.py
@@ -23,7 +23,7 @@ def get_bot_response(message_list):
         model="gpt-3.5-turbo",
         messages=[
                      {"role": "system",
-                      "content": system_prompt + "you always replay in " + language},
+                      "content": system_prompt + "you always replay in {language}"},
                  ] + message_list
     )
     bot_message = gpt3_response["choices"][0]["message"]["content"].strip()
