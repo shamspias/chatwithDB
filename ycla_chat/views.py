f6a897dc1c2e8f64a47fb30ef24fb9fc50c80788
shamspias <shamsuddinpias0@gmail.com>
2023-05-12 23:04:17 +0600

    add views

--- ycla_chat/views.py
+++ ycla_chat/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Chat
+from .serializers import ChatSerializer
+from .tasks import get_bot_response
+
+
+class ChatView(APIView):
+    """
+    API View for Chat
+    """
+
+    def post(self, request):
+        serializer = ChatSerializer(data=request.data)
+        if serializer.is_valid():
+            user_id = serializer.validated_data['user_id']
+            user_message = serializer.validated_data['user_message']
+
+            # Get the last 10 conversations
+            last_chats = Chat.objects.filter(user_id=user_id).order_by('-timestamp')[:10][::-1]
+            message_list = []
+            for chat in last_chats:
+                message_list.append({"role": "user", "content": chat.user_message})
+                message_list.append({"role": "assistant", "content": chat.bot_message})
+
+            # Start the get_bot_response task
+            bot_message = get_bot_response.delay(user_id, message_list, user_message)
+
+            return Response({'message': 'Chat is being processed'}, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, user_id):
+        chats = Chat.objects.filter(user_id=user_id).order_by('-timestamp')
+        serializer = ChatSerializer(chats, many=True)
+        return Response(serializer.data)
