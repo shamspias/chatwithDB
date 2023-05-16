7c9f256343cb0821cd08f21542b9903c86b0a39a
shamspias <shamsuddinpias0@gmail.com>
2023-05-16 19:12:34 +0600

    add API key

--- ycla_chat/admin.py
+++ ycla_chat/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Chat, CustomPrompt
+from .models import Chat, CustomPrompt, ApiKey
 
 
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_id', 'user_message', 'bot_message', 'timestamp')
     search_fields = ('user_id',)
+
+
+@admin.register(ApiKey)
+class ApiKeyAdmin(admin.ModelAdmin):
+    list_display = ('username', 'key',)
+    search_fields = ('username', 'key',)
 
 
 admin.site.register(Chat, ChatAdmin)
