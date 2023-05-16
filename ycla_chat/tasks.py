75c708f638f4de2b05b69cafa9c05becc216154b
shamspias <shamsuddinpias0@gmail.com>
2023-05-16 16:50:20 +0600

    debug: system prompt

--- ycla_chat/tasks.py
+++ ycla_chat/tasks.py
@@ -7,7 +7,9 @@ openai.api_key = settings.OPENAI_API_KEY
 
 @shared_task
 def get_bot_response(message_list, system_prompt, language):
+    print("_____________) SYSTEM PROMPT (__________________")
     print(system_prompt)
+    print("_____________) END SYSTEM PROMPT (__________________")
     print(language)
     gpt3_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
