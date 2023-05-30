0aae92aeb8f30dca56ef9641b94b7f9e05b6dbba
shamspias <shamsuddinpias0@gmail.com>
2023-05-30 18:58:28 +0600

    response stream

--- ycla_chat/tasks.py
+++ ycla_chat/tasks.py
@@ -9,14 +9,23 @@ model_name = settings.OPENAI_AI_MODEL
 
 @shared_task
 def get_bot_response(message_list, system_prompt, language):
-    gpt3_response = openai.ChatCompletion.create(
+    gpt3_stream_response = openai.ChatCompletion.create(
         model=model_name,
+        stream=True,
         messages=[
                      {"role": "system",
                       "content": f"{system_prompt} you always replay in {language} never replay other then {language} even if your previous conversation in other language still you will replay in {language}"},
                  ] + message_list
     )
 
-    bot_message = gpt3_response["choices"][0]["message"]["content"].strip()
+    response_text = ""
 
-    return bot_message
+    # Collect the streamed responses
+    for i in gpt3_stream_response:
+        if 'choices' in i and i['choices']:
+            if 'delta' in i['choices'][0] and 'content' in i['choices'][0]['delta']:
+                response_text += i['choices'][0]['delta']['content']
+
+    # bot_message = gpt3_response["choices"][0]["message"]["content"].strip()
+
+    return response_text
