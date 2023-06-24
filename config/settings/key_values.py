f3c99079d5ae2c8508f6b44c9f273bcb509096fe
shamspias <shamsuddinpias0@gmail.com>
2023-06-24 20:51:39 +0600

    add CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP

--- config/settings/key_values.py
+++ config/settings/key_values.py
@@ -3,6 +3,7 @@ import os
 # Celery
 BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379')
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
 # Open AI key
 OPENAI_API_KEY = os.getenv('OPEN_AI_KEY')
