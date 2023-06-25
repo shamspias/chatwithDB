562f18e109e2141824d9d22a3479c2c887a83613
shamspias <shamsuddinpias0@gmail.com>
2023-06-25 16:21:34 +0600

    add db admin

--- chatdb/admin.py
+++ chatdb/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import DatabaseConfig
 
-# Register your models here.
+
+class DatabaseConfigAdmin(admin.ModelAdmin):
+    """
+    Model Admin for database
+    """
+    list_display = ('name', 'type', 'username', 'host', 'port')  # Fields to display in list view
+    search_fields = ('name', 'username',)  # Fields to search
+    list_filter = ('type',)  # Fields to filter
+
+    # Hide password in list view
+    def get_list_display(self, request):
+        self.exclude = ('password',)
+        return super(DatabaseConfigAdmin, self).get_list_display(request)
+
+    # Hide password in detail view
+    def get_exclude(self, request, obj=None):
+        self.exclude = ('password',)
+        return super(DatabaseConfigAdmin, self).get_exclude(request, obj)
+
+
+admin.site.register(DatabaseConfig, DatabaseConfigAdmin)
