b5eef14513c8a02eaaade8e267b81f91ab74d8ea
shamspias <shamsuddinpias0@gmail.com>
2023-06-25 16:18:47 +0600

    add database

--- chatdb/models.py
+++ chatdb/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class DatabaseConfig(models.Model):
+    DB_TYPES = [
+        ('POSTGRESQL', 'PostgreSQL'),
+        ('MYSQL', 'MySQL'),
+        # more as necessary
+    ]
+    type = models.CharField(max_length=20, choices=DB_TYPES)
+    name = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    host = models.CharField(max_length=255)
+    port = models.IntegerField()
+    external_info = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
